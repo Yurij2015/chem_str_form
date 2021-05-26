08adfa20c3c533ab94147386cb2effe049ed34d3
Yurij <nabor_2008@i.ua>
2021-05-26 21:05:45 +0300

    get formula from db

---
+++ dbconnect.php
@@ -0,0 +1,3 @@
+<?php
+require("./RedBeanPHP5_7/rb.php");
+R::setup('mysql:host=localhost;port=3306;dbname=chem_f', 'root', 'root');
--- index.php
+++ index.php
@@ -41,6 +41,7 @@
   5 12  1  0  0  0  0
 M  END
 
+
 </script>
 <script id="CO" type="chemical/x-mdl-molfile">
 unnamed
@@ -53,6 +54,7 @@ unnamed
   1  2  1  0  0  0  0
   1  3  1  0  0  0  0
 M  END
+
 </script>
 <body>
 <script>
@@ -81,30 +83,60 @@ M  END
 </body>
 </html>
 <?php
-
+require_once("dbconnect.php");
+$formula = R::load('structure_f', 1);
+$molformat = $formula->molformat;
 ?>
 
 <div id="parent"></div>
 <hr>
-<div id="div1"></div>
+<div id="editor"></div>
 <script>
     const chemViewer = new Kekule.ChemWidget.Viewer(document);
-    // chemViewer.setDimension('500px', '400px');
-    // chemViewer.appendToElem(document.getElementById('parent')).setChemObj(molecule);
     // var cmlData = '<cml xmlns="http://www.xml-cml.org/schema"><molecule id="m1"><atomArray><atom id="a2" elementType="C" x2="7.493264658965051" y2="35.58088907877604"/><atom id="a3" elementType="O" x2="8.186084981992602" y2="35.18088907877604"/><atom id="a1" elementType="C" x2="6.800444335937501" y2="35.18088907877604"/></atomArray><bondArray><bond id="b2" order="S" atomRefs2="a2 a3"/><bond id="b1" order="S" atomRefs2="a2 a1"/></bondArray></molecule></cml>';
-    let cmlData = '<cml xmlns="http://www.xml-cml.org/schema"><molecule id="m1"><atomArray><atom id="a1" elementType="C" x2="11.653417998547942" y2="36.66624132865448"/><atom id="a2" elementType="C" x2="12.346217998547942" y2="36.26624132865448"/><atom id="a3" elementType="C" x2="11.653417998547942" y2="37.466241328654476"/><atom id="a4" elementType="C" x2="10.960617998547942" y2="36.26624132865448"/><atom id="a5" elementType="C" x2="12.346217998547942" y2="35.466241328654476"/><atom id="a6" elementType="C" x2="10.960617998547942" y2="37.866241328654475"/><atom id="a7" elementType="C" x2="10.960617998547942" y2="35.466241328654476"/><atom id="a8" elementType="C" x2="11.653417998547942" y2="35.06624132865448"/><atom id="a9" elementType="C" x2="13.039117998547942" y2="35.06624132865448"/><atom id="a11" elementType="O" x2="10.267817998547942" y2="37.466241328654476"/><atom id="a10" elementType="O" x2="10.960617998547942" y2="38.66624132865448"/><atom id="a12" elementType="C" x2="13.731917998547942" y2="35.466241328654476"/></atomArray><bondArray><bond id="b1" order="S" atomRefs2="a1 a2"/><bond id="b2" order="S" atomRefs2="a1 a3"/><bond id="b3" order="D" atomRefs2="a1 a4"/><bond id="b4" order="D" atomRefs2="a2 a5"/><bond id="b5" order="S" atomRefs2="a3 a6"/><bond id="b6" order="S" atomRefs2="a4 a7"/><bond id="b7" order="S" atomRefs2="a5 a8"/><bond id="b8" order="S" atomRefs2="a5 a9"/><bond id="b10" order="S" atomRefs2="a6 a11"/><bond id="b9" order="D" atomRefs2="a6 a10"/><bond id="b11" order="D" atomRefs2="a7 a8"/><bond id="b12" order="D" atomRefs2="a9 a12"/></bondArray><metaDataList><metaData name="generator" content="Kekule"/><metaData name="date" content="@Sat Feb 07 2015 07:26:58 GMT+0300 (Moscow Standard Time)"/></metaDataList></molecule></cml>';
-    let myMolecule = Kekule.IO.loadFormatData(cmlData, 'cml');
+    // let cmlData = '<cml xmlns="http://www.xml-cml.org/schema"><molecule id="m1"><atomArray><atom id="a1" elementType="C" x2="11.653417998547942" y2="36.66624132865448"/><atom id="a2" elementType="C" x2="12.346217998547942" y2="36.26624132865448"/><atom id="a3" elementType="C" x2="11.653417998547942" y2="37.466241328654476"/><atom id="a4" elementType="C" x2="10.960617998547942" y2="36.26624132865448"/><atom id="a5" elementType="C" x2="12.346217998547942" y2="35.466241328654476"/><atom id="a6" elementType="C" x2="10.960617998547942" y2="37.866241328654475"/><atom id="a7" elementType="C" x2="10.960617998547942" y2="35.466241328654476"/><atom id="a8" elementType="C" x2="11.653417998547942" y2="35.06624132865448"/><atom id="a9" elementType="C" x2="13.039117998547942" y2="35.06624132865448"/><atom id="a11" elementType="O" x2="10.267817998547942" y2="37.466241328654476"/><atom id="a10" elementType="O" x2="10.960617998547942" y2="38.66624132865448"/><atom id="a12" elementType="C" x2="13.731917998547942" y2="35.466241328654476"/></atomArray><bondArray><bond id="b1" order="S" atomRefs2="a1 a2"/><bond id="b2" order="S" atomRefs2="a1 a3"/><bond id="b3" order="D" atomRefs2="a1 a4"/><bond id="b4" order="D" atomRefs2="a2 a5"/><bond id="b5" order="S" atomRefs2="a3 a6"/><bond id="b6" order="S" atomRefs2="a4 a7"/><bond id="b7" order="S" atomRefs2="a5 a8"/><bond id="b8" order="S" atomRefs2="a5 a9"/><bond id="b10" order="S" atomRefs2="a6 a11"/><bond id="b9" order="D" atomRefs2="a6 a10"/><bond id="b11" order="D" atomRefs2="a7 a8"/><bond id="b12" order="D" atomRefs2="a9 a12"/></bondArray><metaDataList><metaData name="generator" content="Kekule"/><metaData name="date" content="@Sat Feb 07 2015 07:26:58 GMT+0300 (Moscow Standard Time)"/></metaDataList></molecule></cml>';
+    //     let cmlData = `
+    // unnamed
+    //   Kekule  01071507262D
+    //   0  0  0     0  0              0 V3000
+    // M  V30 BEGIN CTAB
+    // M  V30 COUNTS 12 12 0 0 0
+    // M  V30 BEGIN ATOM
+    // M  V30 1 C 11.653418 36.666241 0.000000 0
+    // M  V30 2 C 12.346218 36.266241 0.000000 0
+    // M  V30 3 C 11.653418 37.466241 0.000000 0
+    // M  V30 4 C 10.960618 36.266241 0.000000 0
+    // M  V30 5 C 12.346218 35.466241 0.000000 0
+    // M  V30 6 C 10.960618 37.866241 0.000000 0
+    // M  V30 7 C 10.960618 35.466241 0.000000 0
+    // M  V30 8 C 11.653418 35.066241 0.000000 0
+    // M  V30 9 C 13.039118 35.066241 0.000000 0
+    // M  V30 10 O 10.267818 37.466241 0.000000 0
+    // M  V30 11 O 10.960618 38.666241 0.000000 0
+    // M  V30 12 C 13.731918 35.466241 0.000000 0
+    // M  V30 END ATOM
+    // M  V30 BEGIN BOND
+    // M  V30 1 1 1 2
+    // M  V30 2 1 1 3
+    // M  V30 3 2 1 4
+    // M  V30 4 2 2 5
+    // M  V30 5 1 3 6
+    // M  V30 6 1 4 7
+    // M  V30 7 1 5 8
+    // M  V30 8 1 5 9
+    // M  V30 9 1 6 10
+    // M  V30 10 2 6 11
+    // M  V30 11 2 7 8
+    // M  V30 12 2 9 12
+    // M  V30 END BOND
+    // M  V30 END CTAB
+    // M  END`;
+    let cmlData = <?php echo "`\r" . $molformat . '`'; ?>;
+    let myMolecule = Kekule.IO.loadFormatData(cmlData, 'mol');
+    // let myMolecule = Kekule.IO.loadFormatData(cmlData, 'cml');
     chemViewer.setChemObj(myMolecule);
     let molecule = chemViewer.getChemObj();
-    // chemViewer.setEnableToolbar(true);  // enable the toolbar
-    // chemViewer.setChemObj(mol2k);
-    // var molecule2 = chemViewer.getChemObj();
-
-    let widget = new Kekule.ChemWidget.PeriodicTable(document.getElementById('div1'));
-    // var widget = new Kekule.ChemWidget.PeriodicTable(document);
-    // widget.setEnableSelect(true)
-    //     .setDisplayedComponents(['symbol', 'name', 'atomicNumber'])
-    //     .appendToElem(document.getElementById('parent'));  // append to HTML element
+    let widget = new Kekule.ChemWidget.PeriodicTable(document.getElementById('editor'));
     let resId = 'url(#molResInsideElem)';
     chemViewer.setDimension('800px', '800px');
     chemViewer.appendToElem(document.getElementById('parent')).setChemObj(molecule);
@@ -116,19 +148,4 @@ M  END
     chemViewer.setEnableDirectInteraction(true);
     chemViewer.setEnableEdit(true);
     chemViewer.setToolbarEvokeModes([Kekule.Widget.EvokeMode.ALWAYS]);
-
-
-    // var chemViewer = new Kekule.ChemWidget.Viewer(document.getElementById('div1'));
-
-
 </script>
-<!--<div id="periodicTable1" data-widget="Kekule.ChemWidget.PeriodicTable"-->
-<!--     data-enable-select="true"-->
-<!--     data-displayed-components="['symbol', 'name', atomicNumber']">-->
-<!--</div>-->
-<!--<div id="chemViewer" style="width:500px;height:400px" data-widget="Kekule.ChemWidget.Viewer"-->
-<!--     data-chem-obj="url(#mol)"></div>-->
-<div id="chemViewer" style="width:500px;height:400px" data-widget="Kekule.ChemWidget.Viewer" data-chem-obj="url(#molResInsideElem)"></div>
-
-<div id="chemViewer" style="width:500px;height:400px" data-widget="Kekule.ChemWidget.Viewer" data-chem-obj="url(#CO)
-"></div>
